ec672fede551402d100d7e379eb735aa0199e434
Oleh Haidar <oleh.haidar@gmail.com>
2021-02-08 17:36:51 +0200

    Added visual in storyboard

---
+++ FetchData/HeroViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HeroViewController.swift
+//  FetchData
+//
+//  Created by Oleh Haidar on 08.02.2021.
+//
+
+import UIKit
+
+class HeroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
---
+++ FetchData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FetchData
+//
+//  Created by Oleh Haidar on 08.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
